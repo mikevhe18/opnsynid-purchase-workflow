391112b9b7f56b232ca1d7cab1eb517683173cc8
mike <viriyananda.michael@gmail.com>
2016-10-25 09:22:37 +0700

    Adding purchase_request_type_to_rfq
    Add purchase_request_type_to_rfq
    
    Add new line on init.py

---
+++ purchase_request_type_to_rfq/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Purchase Request Type To RFQ',
+    'version': '8.0.1.0.0',
+    'summary': 'Insert purchase request type from purchase request lines',
+    'author': 'OpenSynergy Indonesia,Odoo Community Association (OCA)',
+    'website': 'https://opensynergy-indonesia.com',
+    'category': 'Purchase Management',
+    'depends': [
+        'purchase_request_to_rfq'
+    ],
+    'data': [
+        'views/purchase_request_line_make_purchase_order_view.xml'
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+}
